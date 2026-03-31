6c7ec700f9cdc85efae1fdcb489f0ba85ef5292b
hophuoc4so9 <hophuoc4so9@gmail.com>
2026-03-31 15:30:33 +0700

    lab 1

---
+++ hotuanphuoc_2224802010872_lab1_2/bin/hotuanphuoc_2224802010872_lab1_2.dart
@@ -0,0 +1,61 @@
+import 'dart:ffi';
+
+import 'package:hotuanphuoc_2224802010872_lab1_2/hotuanphuoc_2224802010872_lab1_2.dart' as hotuanphuoc_2224802010872_lab1_2;
+
+void main(List<String> arguments) {
+  var name = 'Scirivas';
+  int age = 20;
+  double height = 5.9;
+  bool isAdult = (age > 18 ? true : false);
+  
+  print( 'Name: $name');
+  print( 'Age: $age');
+  print( 'Height: $height');
+
+  // conditional statement
+  if(isAdult) {
+    print('$name is an adult.');
+  } else {
+    print('$name is not an adult.');
+  }
+
+  // loop
+  for (var i = 0; i <= 5; i++) {
+    print('Loop iteration: $i');
+    if(i==3) break;
+  }
+  List<String> friends = ['Bhanu', 'Amar', 'Amulya','Kiran','Sandeep', 'Phước'];
+
+  for(String friend in friends) {
+    print('Friend: $friend');
+  }
+   
+
+  greet(name);
+  var person = Person(name, age, height);
+  person.introduce();
+
+
+  try {
+    int result = 10 ~/ 0; 
+    print('Result: $result');
+  } catch (e) {
+    print('An error occurred: $e');
+  }
+}
+
+void greet(String name) {
+  print('Welcome to SDC, $name!');
+}
+class Person {
+  String name;
+  int age;
+  double height;
+  bool isAdult;
+  Person(this.name, this.age, this.height) : isAdult = age > 18;
+  
+
+  void introduce() {
+    print('Hi, I am $name and I am $age years old and my height is $height feet.');
+  }
+}
---
+++ hotuanphuoc_2224802010872_lab1_2/test/hotuanphuoc_2224802010872_lab1_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:hotuanphuoc_2224802010872_lab1_2/hotuanphuoc_2224802010872_lab1_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
